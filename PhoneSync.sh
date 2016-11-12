9963969405bae7ef39521a624728a451ef82133f
Istvan Steiner <istvan.steiner@ericsson.com>
2016-11-12 08:51:25 +0100

    Workaround for fake error 'Phone is not mounted'

--- PhoneSync.sh
+++ PhoneSync.sh
@@ -5,6 +5,7 @@
 ACTUAL_DATE=$(date +%Y%m%d)
 ACTUAL_TIME=$(date +%H%M%S)
 START_TIME_SEC=$(date +'%s')
+RESIDENT_MEMORY_THRESHOLD=10000
 
 SCRIPT_PATH=$(readlink -f $0)             # In case we execute it as a symlink
 SCRIPT_NAME=$(basename ${SCRIPT_PATH%%.sh})
@@ -25,7 +26,6 @@ PHONE_DOC_DIR="${PHONE_BASE_DIR}/Documents"
 PHONE_TRANSFER_DIR="${PHONE_DOC_DIR}/0_Transfer"
 
 DIRS_TO_PHONE="Private/0_Privat/Projektek/0_Folyo/HazFelujitas_2015- Private/6_AlkalmazottTudomany Private/0_Privat/Aktualis/CsinalniValo Private/0_Privat/Ingatlanok Common/Scripts"
-# DIRS_TO_PHONE="Temp/TestFiles"
 DIRS_FROM_PHONE="DCIM Download Documents/Actual Documents/1_BackTransfer"
 
 gvfs_mtp_mount_dev=$(gvfs-mount -l | awk '/^Mount.*mtp\ -/ {print $4}')
@@ -33,22 +33,28 @@ pid_gvfsd_mtp=$(pidof gvfsd-mtp)
 
 nr_of_transactions=0
 
-function increase_nr_of_transactions {
-    let nr_of_transactions=nr_of_transactions+1
-    cents=$(echo "$nr_of_transactions % 20" | bc)
-    if [ $cents -eq 0 ]
-    then
+function check_resident_memory {
         resident_memory=$(/usr/bin/top -b -p $pid_gvfsd_mtp -n 1 | awk "/$USER/"' {print $6}')
-        print_and_log "${nr_of_transactions}    ${resident_memory}"
-        if [ $resident_memory -gt 10000 ]
+        if [ $resident_memory -gt $RESIDENT_MEMORY_THRESHOLD ]
         then
-            echo -e "WARNING! Resident memory is over 10000.\nRemounting device..."
+            only_log "WARNING! Resident memory is over 10000.\nRemounting device..."
             gvfs-mount -u ${gvfs_mtp_mount_dev}
-            sleep 5
+            sleep 2
             gvfs-mount ${gvfs_mtp_mount_dev}
             pid_gvfsd_mtp=$(pidof gvfsd-mtp)
-            print_and_log "PID OF GVFSD-MTP: $pid_gvfsd_mtp"
+            sleep 2
+            only_log "PID OF GVFSD-MTP: $pid_gvfsd_mtp"
         fi
+}
+
+# Dirty fix for gvfsd-mtp crash
+# We check the resident memory every 100 transaction, and if it is over the limit we remount
+function increase_nr_of_transactions {
+    let nr_of_transactions=nr_of_transactions+1
+    cents=$(echo "$nr_of_transactions % 100" | bc)
+    if [ $cents -eq 0 ]
+    then
+        check_resident_memory
     fi
 }
 
@@ -66,7 +72,8 @@ function exit_if_error {
     if [ $1 -ne 0 ]
     then
         resident_memory=$(/usr/bin/top -b -p $pid_gvfsd_mtp -n 1 | awk "/$USER/"' {print $6}')
-        print_and_log "${nr_of_transactions}    ${resident_memory}"
+        only_log "Number os transactions: ${nr_of_transactions}"
+        only_log "Resident memory: ${resident_memory}"
         print_and_log "ERROR occurred. Exiting..."
         exit 1
     fi
@@ -77,7 +84,7 @@ function copy_fromto_phone {
     local source_dir=$2
     local target_dir=$3
 
-    OIFS="$IFS"
+    OIFS="$IFS"         # Fix for spaces in filenames
     IFS=$'\n'
     for dir in $(find ${source_base_dir}/${source_dir} -type d)
     do
@@ -85,7 +92,6 @@ function copy_fromto_phone {
         only_log "gvfs-mkdir -p ${target_dir}/${dir_tail}"
         gvfs-mkdir -p ${target_dir}/${dir_tail} 2>>${ERRORLOG}
         exit_if_error $?
-#        increase_nr_of_transactions
     done
 
     for file_to_copy in $(find ${source_base_dir}/${source_dir} -type f)
@@ -109,6 +115,8 @@ then
     mkdir -p "${LOGDIR}"
 fi
 
+ls ${PHONE_BASE_DIR} >/dev/null 2>&1    # Fix for not able to see mount for first time
+sleep 1
 if ! ls ${PHONE_BASE_DIR} >/dev/null 2>&1
 then
     echo "ERROR! Phone is not mounted. Please mount it and try again. Exiting..."
@@ -123,9 +131,7 @@ then
     exit_if_error $?
 fi
 
-print_and_log "PID OF GVFSD-MTP: $pid_gvfsd_mtp"
-print_and_log "Nr. of transactions      Resident memory"
-    
+only_log "PID OF GVFSD-MTP: $pid_gvfsd_mtp"
 print_and_log "Copying files from phone to the own HDD..."
 
 for dir in ${DIRS_FROM_PHONE}
@@ -141,17 +147,21 @@ do
     copy_fromto_phone ${PC_DOC_DIR} ${dir} ${PHONE_TRANSFER_DIR}
 done
 
-print_and_log "Searching for duplicate files and removing found duplicate files in the copied files/direcories on the HDD..."
-only_log "cd /home/ethsri/Documents/Common/Scripts/DupliSeek"
+print_and_log "Searching for duplicate files, and removing found duplicate files in the copied files/directories on the HDD..."
 only_log "./dupliSeek.py -v -p -r ${PC_DOC_DIR} ${PC_PHONE_ACTUAL_DIR}"
 
 cd /home/ethsri/Documents/Common/Scripts/DupliSeek
 ./dupliSeek.py -v -p -r ${PC_DOC_DIR} ${PC_PHONE_ACTUAL_DIR} >>${OUTLOG} 2>>${ERRORLOG}
 
-print_and_log "Deleting zero size files and empty directories in the copied files/direcories on the HDD..."
+print_and_log "Deleting zero size files and empty directories in the copied files/directories on the HDD..."
 only_log "find ${PC_PHONE_ACTUAL_DIR} -type f -size 0 -delete"
 only_log "find ${PC_PHONE_ACTUAL_DIR} -type d -empty -delete"
 
 find ${PC_PHONE_ACTUAL_DIR} -type f -size 0 -delete 2>>${ERRORLOG}
 find ${PC_PHONE_ACTUAL_DIR} -type d -empty -delete 2>>${ERRORLOG}
 
+# Clean up errorlog if empty
+if [ ! -s ${ERRORLOG} ]
+then
+    rm -f ${ERRORLOG}
+fi
